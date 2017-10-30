69dc3541dd9e813e0d9279a221c71df7048040b9
Richard Hagen <richard.hagen@gmail.com>
2017-10-30 11:00:54 +0100

    cleanup

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,10 +4,8 @@ namespace AppBundle\Controller;
 
 use AppBundle\Forms\Signup\ColleagueDTO;
 use AppBundle\Forms\Signup\ColleagueType;
-use AppBundle\Forms\Signup\RegisterColleagueType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -31,7 +29,7 @@ class DefaultController extends Controller
      * @Route("/pass")
      */
     public function testSimplePassAction() {
-        $form = $this->createForm(ColleagueType::class,new ColleagueDTO(0, "Test", "test@test.com", 0));
+        $form = $this->createForm(ColleagueType::class,new ColleagueDTO(0, 'Test', 'test@test.com', 0));
 
         return $this->render('default/index.html.twig', [
             'form' => $form->createView(),
